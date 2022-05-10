98dd4213e57370aefce448f57985ced6927a9cdf
Christopher Samson <samsoncr@vcu.edu>
2022-05-10 02:08:53 -0400

    moved connection code to it's own file

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "cmsc508.com";
+$username = "samsoncr";
+$password = "V00905684";
+$database = "project_samsoncr";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$db_success = $conn->select_db($database);
+if (!$db_success) {
+    die("failed to connect to $database");
+}
--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <?php
-$servername = "cmsc508.com";
-$username = "samsoncr";
-$password = "V00905684";
-$database = "project_samsoncr";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$db_success = $conn->select_db($database);
-if (!$db_success) {
-    die("failed to connect to $database");
-}
+require_once('connection.php');
 
 $sql = "SELECT * FROM champions";
 $result = $conn->query($sql);
 while($row = $result->fetch_assoc()) {
     echo $row["champion_name"] . " " . $row["armor"] . "<br>";
 }
+
 ?>
