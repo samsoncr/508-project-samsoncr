acd88c457ea23ffc1d4fce46ccef8833283ba4e1
Christopher Samson <samsoncr@vcu.edu>
2022-05-10 01:21:09 -0400

    add sql connection code

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<body>
-    Foo bar
-</body>
 <?php
-    echo("hello world");
+$servername = "cmsc508.com";
+$username = "samsoncr";
+$password = "V00905684";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
