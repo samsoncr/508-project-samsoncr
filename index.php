c58103746d3133a469606548ec522f70aa184602
Christopher Samson <samsoncr@vcu.edu>
2022-05-10 01:54:06 -0400

    read from database

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $servername = "cmsc508.com";
 $username = "samsoncr";
 $password = "V00905684";
+$database = "project_samsoncr";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -10,5 +11,15 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$db_success = $conn->select_db($database);
+if (!$db_success) {
+    die("failed to connect to $database");
+}
+
+$sql = "SELECT * FROM champions";
+$result = $conn->query($sql);
+while($row = $result->fetch_assoc()) {
+    echo $row["champion_name"] . " " . $row["armor"] . "<br>";
+}
 ?>
