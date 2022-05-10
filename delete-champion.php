03a004e12b9ac378eb14416712104731d4271f7c
Christopher Samson <samsoncr@vcu.edu>
2022-05-10 15:17:49 -0400

    updated delete-champion page. Deletions now go through

--- delete-champion.php
+++ delete-champion.php
@@ -2,18 +2,18 @@
 
 require_once('connection.php');
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+// if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 
-    echo "<form method='post' action='delete-champion.php'>";
-    echo "<table style='border: solid 1px black;'>";
-    echo "<tbody>";
-    echo "<tr><td>Champion Name</td><td><input name='champion_name' type='text' size='25'></td></tr>";
+//     echo "<form method='post' action='delete-champion.php'>";
+//     echo "<table style='border: solid 1px black;'>";
+//     echo "<tbody>";
+//     echo "<tr><td>Champion Name</td><td><input name='champion_name' type='text' size='25'></td></tr>";
     
-    echo "<tr><td></td><td><input type='submit' value='Submit'></td></tr>";
+//     echo "<tr><td></td><td><input type='submit' value='Submit'></td></tr>";
     
-    echo "</tbody>";
-    echo "</table>";
-    echo "</form>";
+//     echo "</tbody>";
+//     echo "</table>";
+//     echo "</form>";
 
 if (!isset($_GET['champion_name']) && $_SERVER['REQUEST_METHOD'] != 'POST') {
     
@@ -21,21 +21,24 @@ if (!isset($_GET['champion_name']) && $_SERVER['REQUEST_METHOD'] != 'POST') {
     $stmt = $conn->prepare("SELECT champion_name FROM champions ORDER BY champion_name");
     $stmt->execute();
     
-    echo "<form method='get'>";
-    echo "<select name='champion_name' onchange='this.form.submit();'>";
+    echo "<form method='post'>";
+    echo "<select name='champion_name'>";
     echo "<option value='0' selected disabled>Select a champion</option>";
     
     $result = $stmt->get_result();
     while ($row = $result->fetch_assoc()) {
         echo "<option value='$row[champion_name]'>$row[champion_name]</option>";
     }
+
+    // echo "<tr><td></td><td><input type='submit' value='Submit'></td></tr>";
     
     echo "</select>";
+    echo "<tr><td></td><td><input type='submit' value='Delete'></td></tr>";
     echo "</form>";
     exit();
 }
 
-} else {
+else {
     
     // try {
     $stmt = $conn->prepare("DELETE FROM champions WHERE champion_name = ?;");
